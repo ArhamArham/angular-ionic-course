5af26f267f8f504d55980d0bdc94a4ffed3f5393
arhamroshanar <arhamroshan445@gmail.com>
2021-01-11 22:49:55 +0500

    google map done

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-    production: false
+    production: false,
+    googleMapsAPIKey: 'AIzaSyDZgC0n-O52qMs0H0mA_OFolVFfNDUrgj8'
 };
 
 /*
